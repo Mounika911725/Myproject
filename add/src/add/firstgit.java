45a4c6a789f2210caa852798d0fe73a1909367ac
G.SIVAREDDY <G.SIVAREDDY@SIVAREDDY>
2022-03-03 07:57:00 +0530

    first commit

---
+++ add/src/add/firstgit.java
@@ -0,0 +1,24 @@
+package add;
+import java.util.Scanner;
+
+public class firstgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+		
+	}
+
+}
+
+	
