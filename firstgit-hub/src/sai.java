e28b9ac8bfd31b879a6f9bf2c09d622efed09165
G.SIVAREDDY <G.SIVAREDDY@SIVAREDDY>
2022-03-03 08:38:00 +0530

    commit

---
+++ firstgit-hub/src/sai.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class sai {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+
+	}
+
+}
